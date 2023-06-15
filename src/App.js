042e64b6d0c207865f701262af79ec0381a763f6
Prashant Pandey <prashantpandey5959@gmail.com>
2023-06-15 22:51:19 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Counter App</h1>
+      <CounterApp />
+    </>
   );
 }
 
+function CounterApp() {
+  // let counter = 1;
+  let [counter, setCounter] = useState(1);
+
+
+  let likeMeAction = () => {
+    counter = counter + 1;
+  console.log("I am button click", counter) ;
+
+    setCounter(counter);
+  };
+  return (
+    <>
+      <h1>{counter}</h1>
+      <input type="button" value="Like Me" onClick={likeMeAction} />
+    </>
+  );
+}
 export default App;
